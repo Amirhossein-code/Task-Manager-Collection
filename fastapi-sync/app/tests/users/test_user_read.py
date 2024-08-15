d8f5c3958e43ce365183cd936cf7415584f3406d
Amir <edadiamirhossein@gmail.com>
2024-08-15 11:03:30 -0400

    Implemented new test : logged in inactive users can not access protected data.

--- fastapi-sync/app/tests/users/test_user_read.py
+++ fastapi-sync/app/tests/users/test_user_read.py
@@ -1,3 +1,6 @@
+from ...models import User
+
+
 class TestUserRead:
     def test_logged_in_user_gets_back_their_info_returns_200(
         self, client, create_user_with_token
@@ -14,4 +17,31 @@ class TestUserRead:
         assert data["email"] == email
         assert data["full_name"] == full_name
         assert data["is_active"]
+
+    def test_logged_in_inactive_user_tries_getting_back_their_info_returns_400(
+        self, client, create_user_with_token, db_session, create_access_token
+    ):
+        # Step 1: Create a user and obtain an access token
+        email = "user969@gmail.com"
+        password = "90@donER@232md"
         
+        create_user_with_token(email=email, password=password)
+
+        # Step 2: Update the user's _is_active field to false
+        user = db_session.query(User).filter(User.email == email).first()
+        if user:
+            user.is_active = False
+            db_session.commit()
+
+        response_access_token = create_access_token(email=email, password=password)
+        response_access_token_json = response_access_token.json()
+        in_active_access_token = response_access_token_json["access_token"]
+
+        # Step 3: Try accessing the user's information again
+        res = client.get(
+            "/users/me", headers={"Authorization": f"Bearer {in_active_access_token}"}
+        )
+
+        # Step 4: Assert that the response returns a 400 status code
+        assert res.status_code == 400
+        assert res.json() == {"detail": "Inactive user"}
